a66a190d9dee748affe2f3dcc2d82696f8ad5ac0
Danil <danil.ignashov@student.ukf.sk>
2025-11-22 11:09:13 +0100

    tailwind

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,8 +1,12 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+
+import "@/styles.css"
+</script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
+  <h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1>  <p>
     Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
     documentation
   </p>
